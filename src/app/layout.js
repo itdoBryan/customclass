b27d3d69493ee6cddf28f051d9b5f9e5434e306c
itdoBryan <bryan@itdo.com>
2024-05-02 05:46:40 +0200

    next auth

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "../../styles/globals.css";
 import {Header} from "./components/sections/Header"
-
+import { NextAuthProvider} from "./components/providers/NextAuthProvider"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,8 +14,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header/>
-        {children}
+        <NextAuthProvider>
+          <Header/>
+          {children}
+        </NextAuthProvider>
       </body>
     </html>
   );
