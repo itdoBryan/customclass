88b1cc5c60be17a9a2e9bca7c0250a146367876a
itdoBryan <bryan@itdo.com>
2024-05-02 09:44:23 +0200

    Update GoogleLogin.jsx

--- src/app/components/buttons/GoogleLogin.jsx
+++ src/app/components/buttons/GoogleLogin.jsx
@@ -3,6 +3,6 @@ import Link from "next/link"
 import { signIn } from "next-auth/react"
 export function GoogleLogin(){
     return(
-        <button onClick={()=>{signIn()}} className="w-[50%] bg-[#ff0000] text-white border-white border-[1px] py-[10px] justify-center items-center rounded-[10px] flex items-center"> <span className="icon-google-plus mr-[10px] text-[20px]"/> Google</button>
+        <button onClick={()=>{signIn("google")}} className="w-[50%] bg-[#ff0000] text-white border-white border-[1px] py-[10px] justify-center items-center rounded-[10px] flex items-center"> <span className="icon-google-plus mr-[10px] text-[20px]"/> Google</button>
     )
 }
