37f38fec5a71b10d3578b7d7297bacddc715892b
itdoBryan <bryan@itdo.com>
2024-05-31 14:55:38 +0200

    cambios en stripe

--- src/app/api/checkout/route.js
+++ src/app/api/checkout/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import {Stripe} from 'stripe'
 
 export async function POST (request){
-    const stripe = new Stripe(process.env.SECRET_KEY);
+    const stripe = new Stripe("sk_test_51PIj1rH3wmKgI5nzxfQZ4P1wzK7amN3FuopPoedCWT3LA0Z58rG6TCKbXDgiot8cZGi18glwtTL3NMLgBWqeYke70028qZYKhI");
     const {id} = await request.json()
     const sesion = await stripe.checkout.sessions.create({
         mode: 'payment',
